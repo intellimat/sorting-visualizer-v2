86c0beff9ad9dc26c17d6722411e9aec40f7f4e0
Matteo <matnice23@gmail.com>
2023-02-02 18:43:18 +0100

    Added history for showing sorting

--- src/sorting-algorithms/quickSort.ts
+++ src/sorting-algorithms/quickSort.ts
@@ -2,25 +2,38 @@ import { swap } from "../utils";
 
 // Optimized quickSort! The pivot is randomized we partition each time the array into three sets!
 export function quickSort(array: number[]): number[][] {
-  // temporary
-  return [];
-  if (Array.isArray(array)) quickSort_recursive(array, 0, array.length - 1);
+  const history: number[][] = [];
+  if (Array.isArray(array))
+    quickSort_recursive(array, 0, array.length - 1, history);
+  return history;
 }
 
-function quickSort_recursive(array: number[], left: number, right: number) {
+function quickSort_recursive(
+  array: number[],
+  left: number,
+  right: number,
+  history: number[][]
+) {
   if (left < right) {
-    randomizePivot(array, left, right);
+    randomizePivot(array, left, right, history);
 
-    const indeces = partition(array, left, right);
+    const indeces = partition(array, left, right, history);
     const m1 = indeces.m1;
     const m2 = indeces.m2;
 
-    quickSort_recursive(array, left, m1);
-    quickSort_recursive(array, m2, right);
+    quickSort_recursive(array, left, m1, history);
+    quickSort_recursive(array, m2, right, history);
+  } else if (left === right) {
+    history.push([...array]);
   }
 }
 
-function partition(array: number[], left: number, right: number) {
+function partition(
+  array: number[],
+  left: number,
+  right: number,
+  history: number[][]
+) {
   const pivot = array[right];
 
   let i = left;
@@ -28,6 +41,7 @@ function partition(array: number[], left: number, right: number) {
   let j = left;
 
   while (j <= k) {
+    history.push([...array]);
     if (array[j] < pivot) {
       swap(array, i, j);
       i++;
@@ -46,7 +60,12 @@ function partition(array: number[], left: number, right: number) {
   return indeces;
 }
 
-function randomizePivot(array: number[], left: number, right: number) {
+function randomizePivot(
+  array: number[],
+  left: number,
+  right: number,
+  history: number[][]
+) {
   const mid = Math.floor((left + right) / 2);
 
   const first = array[left];
@@ -66,6 +85,7 @@ function randomizePivot(array: number[], left: number, right: number) {
     swapping_index = mid;
   else if (median === first) swapping_index = right;
   else if (median === last) swapping_index = left;
-
+  history.push([...array]);
   swap(array, swapping_index, right);
+  history.push([...array]);
 }
